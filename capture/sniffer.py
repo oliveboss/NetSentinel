775a40d195501c3df3e5356bbebf3edec14fdef4
oliveboss <olivierkoffitseamouzou@gmail.com>
2026-04-14 16:17:50 -0400

    correction bug

--- capture/sniffer.py
+++ capture/sniffer.py
@@ -1,19 +1,31 @@
 # capture/sniffer.py
 
-from scapy.all import sniff, IP, TCP, UDP
+from scapy.all import sniff, IP, TCP, UDP, ICMP
 import state.runtime_state as state
 
 def process_packet(packet):
     if not state.packet_callback:
         return
 
+    # Extraction des flags TCP
+    flags = ""
+    if TCP in packet:
+        tcp_flags = packet[TCP].flags
+        if tcp_flags & 0x02: flags += "S"  # SYN
+        if tcp_flags & 0x10: flags += "A"  # ACK
+        if tcp_flags & 0x01: flags += "F"  # FIN
+        if tcp_flags & 0x04: flags += "R"  # RST
+        if tcp_flags & 0x08: flags += "P"  # PSH
+        if tcp_flags & 0x20: flags += "U"  # URG
+
     pkt = {
         "src": packet[IP].src if IP in packet else "-",
         "dst": packet[IP].dst if IP in packet else "-",
-        "proto": "TCP" if TCP in packet else "UDP" if UDP in packet else "OTHER",
-        "sport": packet[TCP].sport if TCP in packet else packet[UDP].sport if UDP in packet else "-",
-        "dport": packet[TCP].dport if TCP in packet else packet[UDP].dport if UDP in packet else "-",
-        "size": len(packet)
+        "proto": "TCP" if TCP in packet else "UDP" if UDP in packet else "ICMP" if ICMP in packet else "OTHER",
+        "sport": packet[TCP].sport if TCP in packet else packet[UDP].sport if UDP in packet else 0,
+        "dport": packet[TCP].dport if TCP in packet else packet[UDP].dport if UDP in packet else 0,
+        "size": len(packet),
+        "flags": flags if TCP in packet else ""
     }
 
     state.packet_callback(pkt)
